c089ed9e5f1baf6252cb43de50c651e96acde9ec
froschs <froschs@my.wccc.edu>
2016-02-29 14:07:28 -0500

    Looks like this works, but had to look up how to access array out of function scope.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 namespace config_reader {
 
-    std::string *read_config(std::string config_file_path) {
+    std::string* read_config(std::string config_file_path) {
         int file_size = 0;
         std::string line;
         std::ifstream inputFile;
@@ -19,18 +19,16 @@ namespace config_reader {
         inputFile.clear(); // Clears the end of file
         inputFile.seekg(0L, std::ios::beg); // Starts over at the beginning of the file
 
-        std::string config_lines[file_size]; // An array of the size of the amount of lines in the file
+        // Had to use dynamic array to save information through scope of function with size dictated by file_size.
+        std::string *config_lines = new std::string[file_size];
 
         // For loop to store each line from the file to an array in config_lines
         for (int i = 0; i < file_size; i++) {
             inputFile >> config_lines[i];
         }
 
-        std::string *ptr;
-        ptr = config_lines;
-
         inputFile.close();
 
-        return ptr;
+        return config_lines;
     }
 }
--- reader.h
+++ reader.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+
 namespace config_reader {
-std::string* read_config(std::string config_file_path);
+	std::string* read_config(std::string config_file_path);
 }
