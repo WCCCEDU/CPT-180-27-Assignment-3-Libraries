1eee22874f65d2852665ad28ab163894b00c5954
Michael Watkins <watkinsmi@my.wccc.edu>
2016-03-02 01:56:17 -0500

    I just need to add link library path, which I'm not sure what should go here.

---
+++ lib/reader.cpp
@@ -0,0 +1,32 @@
+
+#include <string>
+#include "reader.h"
+#include <fstream>
+
+using namespace std;
+
+string* config_reader::read_config(string config_file_path)
+{
+   string* arr;
+
+    ifstream inFile(config_file_path);
+
+    int count = 0;
+    string line;
+    for (count = 0; getline(inFile, line); ++count);
+
+    arr = new string[count];
+    inFile.close();
+
+    inFile.open(config_file_path);
+
+    for (int i = 0; i < count; i++){
+        getline(inFile, line);
+        arr[i] = line;
+    }
+
+    inFile.close();
+
+    return arr;
+
+}
