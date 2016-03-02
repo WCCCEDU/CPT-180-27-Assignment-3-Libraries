2425725faa61449379ad489c7b18f170337b7a3d
Samantha Shurie <s_shurie@yahoo.com>
2016-03-02 13:07:55 -0500

    No more errors! Still need to dereference the returned pointer

--- driver/main.cpp
+++ driver/main.cpp
@@ -5,8 +5,11 @@ using std::cout;
 using std::string;
 
 int main() {
-  string *pointer = config_reader::read_config("./Config.txt");
+  string file_path = "C:\\Users\\Samantha\\Documents\\Schoolwork\\Spring2016\\C++\\Assignment3\\driver\\Config.txt";
+  string* pointer = config_reader::read_config(file_path);
   cout << pointer;
+  string file_input[10] = *pointer;
+  cout << file_input;
 
   return 0;
 }
--- lib/main.cpp
+++ lib/main.cpp
@@ -7,31 +7,42 @@ namespace config_reader{
     using std::string;
     using std::cout;
     using std::ifstream;
+    using std::endl;
 
     // define function for reading config files
     string* read_config(string file_path){
+      // Console output used for testing
+
+      // test for beginning of library function
+      cout << "Now running read_config";
+
       // declare input file and open path
-      cout << file_path;
       ifstream config;
+      cout << "config ifstream created" << endl;
       config.open(file_path);
+      cout << "config opened" << endl;
 
       // declare an array to hold 10 lines (5 headers with 5 values)
       string file_input[10];
+      cout << "array created" << endl;
       string line = "";
 
       int i = 0;
       do{
         getline(config, line);
         file_input[i] = line;
-        cout << file_input[i];
+        //test for lines being read correctly from file
+        cout << file_input[i] << endl;
         i++;
       } while(!config.eof());
 
       // file closes after finished reading input
       config.close();
 
-      // Create string pointer variable and assign address of first array element
+      // Create string pointer variable and assign address of array
       string *lines = file_input;
+      // test for pointer address of array
+      cout << lines << endl;
       return lines;
     }
 }
