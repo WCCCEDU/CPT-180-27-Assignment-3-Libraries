6223d7ee931e646deaafb1e0886f28acfdb36dc2
Clayton Checkeye <checkeyec@my.wccc.edu>
2016-03-02 17:51:41 -0500

    WIP Assignment 3

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
-#include <fstream>
 #include "./reader.h"
 
 namespace config_reader {
     using namespace std;
 
     std::string *read_config(std::string config_file_path) {
-
-        std::string count;
-
+        int count = 0;
         std::ifstream inputFile;
         inputFile.open(config_file_path);
 
@@ -17,6 +13,8 @@ namespace config_reader {
         while (inputFile >> count) {
             cout << count << endl;
         }
+
+        inputFile.close();
 
         return 0;
 
