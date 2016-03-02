3c8133b6d6c751744302131c4e69a4e987842bb8
Samantha Shurie <s_shurie@yahoo.com>
2016-03-02 11:55:48 -0500

    still getting exit code 255 but I think it's getting somewhere

--- driver/main.cpp
+++ driver/main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::string;
 
 int main() {
-  string *pointer = config_reader::read_config("Config.txt");
+  string *pointer = config_reader::read_config("./Config.txt");
   cout << pointer;
 
   return 0;
